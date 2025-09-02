de929b29d84186802199134a12dca5cdb744de3d
Aniket Shrivastava <aniketsrivastava57@gmail.com>
2025-09-02 15:20:13 +0530

    first commit

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ const app= express();
 const mongoose =require("mongoose");
 const Listing =require("./models/listing.js");
 const path = require("path")
+const methodOverride =require("method-override")
+
+
+
+// ----------------------------------------------------------
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+// -------------------------------------------------------------
+
+
+
+
 // Connect Database
 const MONGO_URL= "mongodb://127.0.0.1:27017/Hotelbooking";
 
@@ -48,6 +59,21 @@ app.get("/listings/:id", async(req,res)=>{
      res.render("listing/show", { lisiting });
 })
 // ---------------------------------------------------------------
+// Edit ROute
+app.get("/listings/:id/edit",async(req,res)=> {
+    let {id} =req.params;
+    const listing =await Listing.findById(id);
+    res.render("listings/edit", { listing });
+
+})
+// ----------------------------------------------------------------------
+// update route
+app.put("/listings/:id",async(req,res)=> {
+   let {id} =req.params;
+   await Listing .findByIdUodate(id,{...req.body.listing});
+   res.redirect(`/listings/${id}`);
+});
+
 app.listen(8080,()=>{
     console.log("Running succesfully  to port 8080")
 })
