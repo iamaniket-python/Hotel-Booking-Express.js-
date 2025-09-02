8600a7ad124affa6a52c607520d4eee9d32ed3b3
Aniket Shrivastava <aniketsrivastava57@gmail.com>
2025-09-02 11:11:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express=require("express");
+const app= express();
+const mongoose =require("mongoose");
+const Listing =require("./models/listing.js");
+const path = require("path")
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+// Connect Database
+const MONGO_URL= "mongodb://127.0.0.1:27017/Hotelbooking";
+
+main().then(()=>{
+    console.log("Succesfully connected")
+}).catch(err =>{
+    console.log(err);
+});
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+app.get("/",(req, res)=>{
+    res.send("hii sonali");
+})
+
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listing/index", { allListings });
+});
+
+
+
+app.listen(8080,()=>{
+    console.log("Running succesfully  to port 8080")
+})
