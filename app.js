c3206a1f8c53ef49e6439b88fadc5150407394f6
Aniket Shrivastava <aniketsrivastava57@gmail.com>
2025-09-09 11:53:47 +0530

    first commit

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const Listing =require("./models/listing.js");
 const path = require("path")
 const methodOverride =require("method-override")
 const ejsMate=require("ejs-mate");
-
+const wrapAsync =require("./utils/wrapAsync.js");
+const ExpressError =require("./utils/ExpressError.js");
 
 // ----------------------------------------------------------
 app.set("view engine", "ejs");
@@ -21,73 +22,83 @@ app.use(express.static(path.join(__dirname,'/public')))
 
 // Connect Database
 const MONGO_URL= "mongodb://127.0.0.1:27017/Hotelbooking";
-
-main().then(()=>{
-    console.log("Succesfully connected")
-}).catch(err =>{
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 async function main() {
   await mongoose.connect(MONGO_URL);
 }
-// --------------------------------------------------------
-app.get("/",(req, res)=>{
-    res.send("<h1>Hello</h1>");
-})
-// --------------------------------------------------------
-// New listing form
-app.get("/listings/new", (req, res) => {
-    res.render("listing/new");   
+
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
 });
-// ----------------------------------------------------
-// #index route
+
+//Index Route
 app.get("/listings", async (req, res) => {
   const allListings = await Listing.find({});
-  res.render("listing/index", { allListings });
+  res.render("listings/index.ejs", { allListings });
 });
-//  create route
-// ----------------------------------------------------
-app.post("/listings",async(req ,res)=>{
-    const newListing =new Listing(req.body.listing);
-    await newListing.save()
-    res.redirect("/listings");
-});
-// -----------------------------------------------------------
-// show route
-app.get("/listings/:id", async(req,res)=>{
-    let {id} =req.params;
-    const lisiting =await Listing.findById(id);
-    
-     res.render("listing/show", { lisiting });
-})
-// ---------------------------------------------------------------
-// Edit ROute
-app.get("/listings/:id/edit",async(req,res)=> {
-    let {id} =req.params;
-    const listing =await Listing.findById(id);
-    res.render("listing/edit", { listing });
 
-})
-// ----------------------------------------------------------------------
-// update route
-app.put("/listings/:id",async(req,res)=> {
-   let {id} =req.params;
-   await Listing .findByIdAndUpdate(id,{...req.body.listing});
-   res.redirect(`/listings/${id}`);
+//New Route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
 });
-// ------------------------------------------------------------------------
-//  delete
+
+//Show Route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
+//Create Route
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
+
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
+
+//Delete Route
 app.delete("/listings/:id", async (req, res) => {
-    let { id } = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");  
-  
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
 });
 
+// app.get("/testListing", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "My New Villa",
+//     description: "By the beach",
+//     price: 1200,
+//     location: "Calangute, Goa",
+//     country: "India",
+//   });
 
-// ---------------------------------------------------------------------------
-app.listen(8080,()=>{
-    console.log("Running succesfully  to port 8080")
-})
+//   await sampleListing.save();
+//   console.log("sample was saved");
+//   res.send("successful testing");
+// });
+
+app.listen(8080, () => {
+  console.log("server is listening to port 8080");
+});
